a5648d04d039f64c655b15f3459d5b7862e70e65
FrancoLopez0 <francoalelopez@gmail.com>
2025-06-02 22:07:29 -0300

    Segunda consigna de TP3

--- 3_trabajos_practicos/tp3/firmware/firmware.c
+++ 3_trabajos_practicos/tp3/firmware/firmware.c
@@ -11,46 +11,39 @@
 
 #define SDA_PIN 4U
 #define SCL_PIN 5U
-#define LCD_DIR 0x3F 
+#define LCD_DIR 0x27 
 #define INPUT_PIN 7U
 #define AUX_PIN 8U
 
-
 SemaphoreHandle_t InputSemaphoreHandler;
 
-void vPollingUpTask(void *params){
-    
-    uint32_t prev_time = 0;
-    uint32_t period = 0;
-    float frec = 0;
-
-    for(;;){
-
-        if(gpio_get(INPUT_PIN)){
-            xSemaphoreGive(InputSemaphoreHandler);
-            while(gpio_get(INPUT_PIN));
-        }
-    }
-}
-
 void vPrintTask(void *params){
+    char frec_str[25];
+    lcd_set_cursor(0,0);
+    lcd_string("Frecuencia:");
     for(;;){
         vTaskDelay(pdMS_TO_TICKS(200));
-        uint32_t periodos = uxSemaphoreGetCount(InputSemaphoreHandler) ;
-        printf("Frec: %.2f hz\n", (float)periodos / 200e-3);
+        lcd_set_cursor(1,0);
+        uint32_t periodos = uxSemaphoreGetCount(InputSemaphoreHandler);
+        sprintf(frec_str, "%5.0f hz", (float)periodos / 200e-3);
+        lcd_string(frec_str);
         xQueueReset(InputSemaphoreHandler);
-        // xSemaphoreTake(InputSemaphoreHandler, portMAX_DELAY);
     }
 }
 
+void IRQ_Input(uint gpio, uint32_t events){
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+    xSemaphoreGiveFromISR(InputSemaphoreHandler, &xHigherPriorityTaskWoken);
+}
 
 void gpio_config(void){
     gpio_init(INPUT_PIN);
     gpio_set_input_enabled(INPUT_PIN, true);
+    gpio_set_irq_enabled_with_callback(INPUT_PIN, GPIO_IRQ_EDGE_RISE, true, &IRQ_Input);
 }
 
 void i2c_config(void){
-    i2c_init(i2c_default, 100000);
+    i2c_init(i2c0, 100000);
     gpio_set_function(SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(SCL_PIN, GPIO_FUNC_I2C);
 
@@ -58,8 +51,6 @@ void i2c_config(void){
     gpio_pull_up(SCL_PIN);
 
     bi_decl(bi_2pins_with_func(SDA_PIN,SCL_PIN, GPIO_FUNC_I2C));
-
-    lcd_init(i2c0, LCD_DIR);
 }
 
 int main()
@@ -70,18 +61,11 @@ int main()
 
     i2c_config();
 
-    pwm_user_init(AUX_PIN, 1000);
+    pwm_user_init(AUX_PIN, 9881);
 
-    InputSemaphoreHandler = xSemaphoreCreateCounting(1000, 0);
+    lcd_init(i2c0, LCD_DIR);
 
-    xTaskCreate(
-        vPollingUpTask,
-        "PollingUpTask",
-        configMINIMAL_STACK_SIZE,
-        NULL,
-        1,
-        NULL
-    );
+    InputSemaphoreHandler = xSemaphoreCreateCounting(5000, 0);
 
     xTaskCreate(
         vPrintTask,
